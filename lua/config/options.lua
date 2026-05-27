3d097a5ecd52da3abaf34e298bce173f57bcc0a1
Tiago J Pereira <pereira.tiagojoao@gmail.com>
2026-05-27 02:10:19 -0300

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,15 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.spelllang = { "pt_br", "en_us" }
+vim.opt.relativenumber = false
+vim.opt.clipboard = "unnamedplus"
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+  end,
+})
